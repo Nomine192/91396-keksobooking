671840a44ff450196d0bf2078386f1bc7b9353d6
Dmitriy Strungar <dmitriy.st92@mail.ru>
2017-02-11 23:49:30 +0300

    Оптимизировал обработчик с помощью делегирования,добавил взаимодействие с клавиатуры

--- js/form.js
+++ js/form.js
@@ -1,11 +1,35 @@
 'use strict';
 
-var pins = document.querySelectorAll('.pin');
 var dialog = document.querySelector('.dialog');
 var dialogClose = document.querySelector('.dialog__close');
 var noticeForm = document.querySelector('.notice__form');
+var tokyoPinMap = document.querySelector('.tokyo__pin-map');
 
 // Функция проверки и удаления класса
+
+var ENTER_KEY_CODE = 13;
+var ESCAPE_KEY_CODE = 27;
+
+var changeAria = function (element) {
+  var pressed = (element.getAttribute('aria-pressed') === 'true');
+  if (!pressed) {
+    element.setAttribute('aria-pressed', !pressed);
+  }
+};
+
+var isActivate = function (e) {
+  return e.keyCode && e.keyCode === ENTER_KEY_CODE;
+};
+
+var setupKeydownHandler = function (e) {
+  if (e.keyCode === ESCAPE_KEY_CODE) {
+    dialog.style.display = 'none';
+  }
+};
+
+var showSetupElement = function () {
+  document.addEventListener('keydown', setupKeydownHandler);
+};
 
 var pinDelete = function () {
   var pinActive = document.querySelector('.pin--active');
@@ -16,26 +40,36 @@ var pinDelete = function () {
 
 // Добавляем класс
 
-var pinAdd = function (pin) {
+var pinAdd = function (e) {
+  var element = e.target.classList.contains('pin') ? e.target : e.target.parentElement;
   pinDelete();
-  pin.classList.add('pin--active');
+  element.classList.add('pin--active');
   dialog.style.display = 'block';
 };
 
 // Добавляем действие по клику
 
-for (var i = 0; i < pins.length; i++) {
-  pins[i].addEventListener('click', function (e) {
-    pinAdd(e.currentTarget);
-  });
-}
-
 var closeDialog = function () {
   dialog.style.display = 'none';
   pinDelete();
+  changeAria(dialogClose);
 };
 
+
+var listenPress = function (e) {
+  if (isActivate(e)) {
+    pinAdd(e);
+  }
+};
+
+tokyoPinMap.addEventListener('keydown', listenPress);
+
+tokyoPinMap.addEventListener('keydown', function (e) {
+  showSetupElement();
+});
+
 dialogClose.addEventListener('click', closeDialog);
+tokyoPinMap.addEventListener('click', pinAdd);
 
 // Проверяем время заезда и выезда
 
