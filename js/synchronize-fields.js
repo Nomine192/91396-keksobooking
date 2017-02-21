9bf793c8d53293f4a2de076cfd761c5ba0976dc4
Дмитрий <dmitriy.st92@mail.ru>
2017-02-21 23:56:17 +0500

    Изолировал функции
    Сделал инкапсуляцию и исправил ошибки

--- js/synchronize-fields.js
+++ js/synchronize-fields.js
@@ -2,13 +2,13 @@
 
 (function () {
   window.synchronizeFields = (function (elementValue, anotherValueElement, arrayValue, anotherArrayValue, strNameValue) {
-	  elementValue.addEventListener('change', function () {
-	    var selectedValue = arrayValue.indexOf(elementValue.value);
-	    anotherValueElement[strNameValue] = anotherArrayValue[selectedValue];
-	  });
-	  anotherValueElement.addEventListener('change', function () {
-	    var anotherSelecteValue = anotherArrayValue.indexOf(anotherValueElement.value);
-	    elementValue[strNameValue] = arrayValue[anotherSelecteValue];
-	  });
-	});
+    elementValue.addEventListener('change', function () {
+      var selectedValue = arrayValue.indexOf(elementValue.value);
+      anotherValueElement[strNameValue] = anotherArrayValue[selectedValue];
+    });
+    anotherValueElement.addEventListener('change', function () {
+      var anotherSelecteValue = anotherArrayValue.indexOf(anotherValueElement.value);
+      elementValue[strNameValue] = arrayValue[anotherSelecteValue];
+    });
+  });
 })();
