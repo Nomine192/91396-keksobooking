4976e4753c7a6848ae8d037fb54bfecc5a24d38b
Дмитрий <dmitriy.st92@mail.ru>
2017-02-24 23:33:51 +0500

    Добавил отрисовку pin

--- js/initialize-pins.js
+++ js/initialize-pins.js
@@ -1,66 +1,37 @@
 'use strict';
 
-(function () {
-  var ENTER_KEY_CODE = 13;
-  var ESCAPE_KEY_CODE = 27;
+window.initializePins = (function () {
+  var tokyo = document.querySelector('.tokyo__pin-map');
+  var fragment = document.createDocumentFragment();
+  var DATA_URL = 'https://intensive-javascript-server-pedmyactpq.now.sh/keksobooking/data';
+  var similarApartments = [];
 
-  var changeAria = function (element) {
-    var pressed = (element.getAttribute('aria-pressed') === 'true');
-    if (!pressed) {
-      element.setAttribute('aria-pressed', !pressed);
-    }
+  var loadData = function () {
+    window.load(DATA_URL, function (data) {
+      similarApartments = data;
+      renderData();
+    });
   };
 
-  var isActivate = function (e) {
-    return e.keyCode && e.keyCode === ENTER_KEY_CODE;
+  var renderData = function () {
+    var slicedArr = similarApartments.slice(0, 3);
+
+    slicedArr.forEach(function (item) {
+      fragment.appendChild(window.render(item));
+    });
+
+    tokyo.appendChild(fragment);
   };
-
-  var dialog = document.querySelector('.dialog');
-  var dialogClose = document.querySelector('.dialog__close');
-  var tokyoPinMap = document.querySelector('.tokyo__pin-map');
-
-  var deleteClass = function () {
-    var activePin = document.querySelector('.pin--active');
-    if (activePin) {
-      activePin.classList.remove('pin--active');
-    }
+  return {
+    showPin: function (callback) {
+      callback();
+    },
+    deleteClass: function () {
+      var activePin = document.querySelector('.pin--active');
+      if (activePin) {
+        activePin.classList.remove('pin--active');
+      }
+    },
+    loadData: loadData
   };
-
-  var changeClass = function (e) {
-    var targetElement = e.target.classList.contains('pin') ? e.target : e.target.parentElement;
-    deleteClass();
-    targetElement.classList.add('pin--active');
-    dialog.style.display = 'block';
-  };
-
-  var setupKeydownHandler = function (e) {
-    if (e.keyCode === ESCAPE_KEY_CODE) {
-      dialog.style.display = 'none';
-    }
-  };
-
-  var showSetupElement = function () {
-    document.addEventListener('keydown', setupKeydownHandler);
-  };
-
-  var closeDialog = function () {
-    dialog.style.display = 'none';
-    deleteClass();
-    changeAria(dialogClose);
-  };
-
-  var listenPress = function (e) {
-    if (isActivate(e)) {
-      changeClass(e);
-    }
-  };
-
-  tokyoPinMap.addEventListener('keydown', listenPress);
-
-  tokyoPinMap.addEventListener('keydown', function (e) {
-    showSetupElement();
-  });
-
-  dialogClose.addEventListener('click', closeDialog);
-  tokyoPinMap.addEventListener('click', changeClass);
 })();
