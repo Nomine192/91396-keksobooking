708e771b55d323bb6e044a32a34e8013719ec73c
Дмитрий <dmitriy.st92@mail.ru>
2017-02-24 15:04:21 +0500

    Пофиксил ошибку синхронизации типа жилья

--- js/form.js
+++ js/form.js
@@ -42,22 +42,19 @@
 
   tokyoPinMap.addEventListener('keydown', onPinKeyDown);
   tokyoPinMap.addEventListener('click', activatePin);
+  var syncValues = function (element, value) {
+    element.value = value;
+  };
 
-  window.synchronizeFields(formTime, formTimeout, ['12', '13', '14'], ['12', '13', '14'], 'value', function (val) {
-    formTimeout.value = val;
-  });
-  window.synchronizeFields(formTimeout, formTime, ['12', '13', '14'], ['12', '13', '14'], 'value', function (val) {
-    formTime.value = val;
-  });
-  window.synchronizeFields(formType, formPrice, ['1000', '0', '10000'], ['1000', '0', '10000'], 'min', function (val) {
-    formPrice.min = val;
-  });
-  window.synchronizeFields(formRoomNumber, formCapacity, ['1', '2', '100'], ['0', '3', '3'], 'value', function (val) {
-    formCapacity.value = val;
-  });
-  window.synchronizeFields(formCapacity, formRoomNumber, ['0', '3', '3'], ['1', '2', '100'], 'value', function (val) {
-    formRoomNumber.value = val;
-  });
+  var syncValueWithMin = function (element, value) {
+    element.min = value;
+  };
+
+  window.synchronizeFields(formTime, formTimeout, ['12', '13', '14'], ['12', '13', '14'], syncValues);
+  window.synchronizeFields(formTimeout, formTime, ['12', '13', '14'], ['12', '13', '14'], syncValues);
+  window.synchronizeFields(formType, formPrice, ['1000', '0', '10000'], ['1000', '0', '10000'], syncValueWithMin);
+  window.synchronizeFields(formRoomNumber, formCapacity, ['1', '2', '100'], ['0', '3', '3'], syncValues);
+  window.synchronizeFields(formCapacity, formRoomNumber, ['0', '3', '3'], ['1', '2', '100'], syncValues);
   formTitle.required = true;
   formTitle.minLength = 30;
   formTitle.maxLength = 100;
--- js/synchronize-fields.js
+++ js/synchronize-fields.js
@@ -1,8 +1,10 @@
 'use strict';
 
-window.synchronizeFields = (function (elementValue, anotherValueElement, arrayValue, anotherArrayValue, strNameValue, callback) {
-  elementValue.addEventListener('change', function () {
-    var selectedValue = arrayValue.indexOf(elementValue.value);
-    callback(anotherArrayValue[selectedValue]);
-  });
-});
+window.synchronizeFields = (function () {
+  return function (elementValue, anotherValueElement, arrayValue, anotherArrayValue, callback) {
+    elementValue.addEventListener('change', function () {
+      var selectedValue = arrayValue.indexOf(elementValue.value);
+      callback(anotherValueElement, anotherArrayValue[selectedValue]);
+    });
+  };
+})();
